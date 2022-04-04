0a6008c5684f4b16e9276b85db5be5beeedccc7a
Noah Shaffir <92744167+noahshaffir@users.noreply.github.com>
2022-04-04 19:00:21 -0400

    made the thing actually work for creating directories, since it wasnt working beforehand

--- collect-matching-filenames.py
+++ collect-matching-filenames.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 # Glob pattern tester tool (to help test patterns to look for)
 # https://www.digitalocean.com/community/tools/glob?comments=true&glob=%2Ftemplates%2F%2A%2A%2F%2A%2B%28.js%7C.html%29&matches=false&tests=%2F%2F%20These%20will%20all%20match%20as%20the%20globstar%20matches%20zero%20or%20more%20directories%20recursively&tests=%2Ftemplates%2Fcrud%2Fbast.html&tests=%2Ftemplates%2Fbast.html&tests=%2Fcrud%2Fbast.html&tests=%2Ffile.js&tests=%2Fone%2Ffile.js&tests=%2Fone%2Ftwo%2Ffile.js&tests=%2Fone%2Ftwo%2Fthree%2Ffile.js
@@ -18,27 +19,28 @@ file_patterns = [
     "views.py",
     "forms.py",
     "urls.py",
-    "templates/**/*.html", #Finds all html files in the templates folder
-    # "templates/base.html", #Finds all html files in the templates folder
+    #"templates/**/*.html", #Finds all html files in the templates folder
+    #"templates/base.html", #Finds all html files in the templates folder
 
-    "static/**/*+(.css|.js)", #Finds all js and css files in the static files folder
+    #"static/**/*+(.css|.js)", #Finds all js and css files in the static files folder
 ]
 
-# for path in Path('Pod1RepoTest').rglob('templates/**/*.html'):
+# for path in Path('./').rglob('templates/**/*.html'):
 #     print("Path: ", path.name, path)
 file_paths = {}
 for pattern in file_patterns:
     print("PATTERN: ", pattern)
     if pattern not in file_paths.keys():
-        file_paths[pattern] = Path('Pod1RepoTest').rglob(pattern)
+        file_paths[pattern] = Path(f'./{pattern}')
+        os.mkdir(file_paths[pattern])
     
-    print(f"Paths for {pattern}: ", file_paths[pattern])
+    # print(f"Paths for {pattern}: ", file_paths[pattern])
     # for path in file_paths[pattern]:
-    #     print("Path: ", path.name, path)
+        # print("Path: ", path.name, path)
+        # os.mkdir(path) #we want this line to create a directory for each path
 
 
-# os.mkdir(path)
-# print("Directory '% s' created" % directory)
+#print("Directory '% s' created" % directory)
 
 # for key, value in file_paths.items():
 #     os.mkdir(path)
